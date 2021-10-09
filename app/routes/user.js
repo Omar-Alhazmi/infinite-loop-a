4b83eb03253b36de434c035df614ade37a197074
Omar Alhazmi <vex.xcc@gmail.com>
2021-10-09 11:17:46 +0300

    add storage info with in user registration

--- app/routes/user.js
+++ app/routes/user.js
@@ -2,13 +2,14 @@ const express = require('express');
 const router = express.Router();
 const jwt = require('jsonwebtoken');
 const User = require('../models/User');
-// const Auth = require('../../config/auth');
+const Storage = require('../models/Storage');
+const Auth = require('../../config/auth');
 const config = require('../../config/db');
+const GeneralCapacity = require('../models/GeneralCapacity');
 require('dotenv').config();
 const nodemailer = require('nodemailer');
 const { google } = require("googleapis");
 const OAuth2 = google.auth.OAuth2;
-
 const {
     G_USER,
     CLIENT_ID,
@@ -22,17 +23,17 @@ const oauth2Client = new OAuth2(
 );
 oauth2Client.setCredentials({
     refresh_token: REFRESH_TOKEN
-}); 
- const access_Token = oauth2Client.getAccessToken()
-let transporter = nodemailer
-.createTransport({
-    service: 'Gmail',
-    auth: {
-        type: 'OAuth2',
-        clientId: CLIENT_ID,
-        clientSecret: CLIENT_SECRET
-    }
 });
+const access_Token = oauth2Client.getAccessToken()
+let transporter = nodemailer
+    .createTransport({
+        service: 'Gmail',
+        auth: {
+            type: 'OAuth2',
+            clientId: CLIENT_ID,
+            clientSecret: CLIENT_SECRET
+        }
+    });
 transporter.on('token', token => {
     console.log('A new access token was generated');
     console.log('User: %s', token.user);
@@ -44,18 +45,18 @@ async function sendMail(user) {
     try {
         const port = process.env.PORT || 5000
         let server = ""
-        {process.env.server ? server = `${process.env.server}api/user/activation/${user._id}` : server = `http://localhost:${port}/api/user/activation/${user._id}` }
+        { process.env.server ? server = `${process.env.server}api/user/activation/${user._id}` : server = `http://localhost:${port}/api/user/activation/${user._id}` }
         const mailOptions = {
             from: `StorageChain <${G_USER}>`,
             to: user.Email,
             subject: 'Verify Email',
             text: 'Welcome To Storage Chain Family',
             html: `<center><h1>HI ${user.FullName}</h1><br/><a href=${server}><button Style="border-radius: 12px; background-color:#00bbff;  font-size: 16px; margin: 4px 2px;  border: none; color: white; padding: 20px; text-align: center;width:120px;cursor: pointer;" > Please press Here to verify Your Email</button></center>`,
-            auth : {
-                user         : G_USER,
-                refreshToken : REFRESH_TOKEN,
-                accessToken  : access_Token,
-                expires      : 1494388182480
+            auth: {
+                user: G_USER,
+                refreshToken: REFRESH_TOKEN,
+                accessToken: access_Token,
+                expires: 1494388182480
             }
         };
         const result = await transporter.sendMail(mailOptions, (error, response) => {
@@ -72,38 +73,66 @@ async function sendMail(user) {
 //----------------------- All Post request ----------------------------\\
 //this rout response to register a new user
 router.post('/api/User/register', (req, res) => {
-    const { FullName, CompanyName, NationalId, Phone, Email, password, Role,SubscriptionPlan } = req.body
-    const user = {}
+    let newCapacity
+    GeneralCapacity.find({}, async (err, foundCapacity) => {
+        console.log("====>\n", foundCapacity[0]);
+
+        const { FullName, CompanyName, NationalId, Phone, Email, password, Role, SubscriptionPlan } = req.body
+        const user = {}
         user.FullName = FullName,
-        user.CompanyName = CompanyName,
-        user.NationalId = NationalId,
-        user.Phone = Phone,
-        user.Email = Email,
-        user.password = password,
-        user.Role = Role,
-        user.SubscriptionPlan = SubscriptionPlan
-    const newUser = new User(user)
-    User.addUser(newUser, (err, addedUser) => {
-        console.log(addedUser);
-        if (err) {
-            let message = "";  
-                      console.log(err.errors);
-            if (err.errors.Email) message = "Email ALready Exists";
-            if (err.errors.CompanyName) message = "Please Make Sure To Company Name is required"
-            else message = "Please Make Sure To Fill The Form Accurately";
-            return res.json({
-                success: false,
-                message
-            });
-        } else {
-            sendMail(newUser)
-                .then((result) => console.log('Email sent...', result))
-                .catch((error) => console.log(error.message));
-            res.status(200).json({
-                success: true,
-                message: "User registration is successful."
-            });
+            user.CompanyName = CompanyName,
+            user.NationalId = NationalId,
+            user.Phone = Phone,
+            user.Email = Email,
+            user.password = password,
+            user.Role = Role,
+            user.SubscriptionPlan = SubscriptionPlan
+        const newUser = new User(user)
+        const date = new Date()
+        const stor = {}
+        stor.BelongTo = newUser._id,
+            stor.StorageType = SubscriptionPlan
+        if (SubscriptionPlan === "Primary") {
+            stor.SubscriptionStorageArea = 50
+            stor.StorageArea = 0
+            newCapacity = foundCapacity[0].TotalCapacity - stor.SubscriptionStorageArea
+            await foundCapacity[0].updateOne({ TotalCapacity: newCapacity });
         }
+        else if (SubscriptionPlan === "Premium") {
+            stor.SubscriptionStorageArea = 100
+            stor.StorageArea = 0
+            newCapacity = foundCapacity[0].TotalCapacity - stor.SubscriptionStorageArea
+            await foundCapacity[0].updateOne({ TotalCapacity: newCapacity });
+        } else {
+            stor.StorageArea = 0
+        }
+        stor.EndOfSubscription = SubscriptionPlan === "Primary" ? EndOfSubscription = new Date(date.setMonth(date.getMonth() + 1))
+            : SubscriptionPlan === "Premium" ? EndOfSubscription = new Date(date.setMonth(date.getMonth() + 12))
+                : 0
+        const newStorage = new Storage(stor)
+        newStorage.save()
+        User.addUser(newUser, (err, addedUser) => {
+            console.log(addedUser);
+            if (err) {
+                let message = "";
+                console.log(err.errors);
+                if (err.errors.Email) message = "Email ALready Exists";
+                if (err.errors.CompanyName) message = "Please Make Sure To Company Name is required"
+                // else message = "Please Make Sure To Fill The Form Accurately";
+                return res.json({
+                    success: false,
+                    message
+                });
+            } else {
+                sendMail(newUser)
+                    .then((result) => console.log('Email sent...', result))
+                    .catch((error) => console.log(error.message));
+                res.status(200).json({
+                    success: true,
+                    message: "User registration is successful."
+                });
+            }
+        });
     });
 });
 router.post('/api/User/login', (req, res) => {
@@ -169,7 +198,7 @@ router.get('/api/user/activation/:id', async (req, res) => {
                             return;
                         }
                         else {
-                           res.redirect('https://vex-xcc.github.io/infinite-loop/#/SignIn'); 
+                            res.redirect('https://vex-xcc.github.io/infinite-loop/#/SignIn');
                         }
                     })
             } catch (error) {
