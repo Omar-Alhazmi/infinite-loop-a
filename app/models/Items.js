1ecb65fa19468ce1784c34bef71f00e583611c31
Omar Alhazmi <vex.xcc@gmail.com>
2021-10-09 11:15:18 +0300

    add Items Schema to store the item data

---
+++ app/models/Items.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const ItemsSchema = mongoose.Schema(
+    {
+        ItemName: {
+            type: String,
+            required: [true, "Item Name is required"]
+        },
+        ItemSize: {
+            type: Number,
+            required: [true, "Item Size is required"]
+        },
+        StorAt: {
+            type: Schema.Types.ObjectId,
+            ref: 'Storage',
+            required: [true, "Stor At is required"]
+        }
+    },
+    {
+        timestamps: true,
+    }
+);
+module.exports = new mongoose.model('Items', ItemsSchema)
