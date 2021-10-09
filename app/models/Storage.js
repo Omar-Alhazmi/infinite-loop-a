c96fba71d53ed4e0155237d99aa9216a560e4850
Omar Alhazmi <vex.xcc@gmail.com>
2021-10-09 11:16:10 +0300

    add StorageSchema to store the user storage info

---
+++ app/models/Storage.js
@@ -0,0 +1,37 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const StorageSchema = mongoose.Schema({
+        StorageType: {
+            type: String,
+            required: [true, 'User Type is required'],
+            enum: {
+                values: ["Daily","Primary","Premium"],
+                message: '{VALUE} is not supported',
+            },
+        },
+        SubscriptionStorageArea:{
+            type:Number
+        },
+        StorageArea:{
+            type:Number
+        },
+        SubscriptionDate: {
+            type: Date, default: Date.now
+        },
+        EndOfSubscription: {
+            type: Date
+        },
+        BelongTo: {
+            type: Schema.Types.ObjectId,
+            ref: 'User'
+        },
+        Logo: {
+            type: String
+        },
+        StorageCapacity:{
+            type: Schema.Types.ObjectId,
+            ref: 'GeneralCapacity'
+        }
+    })
+module.exports = new mongoose.model('Storage', StorageSchema)
